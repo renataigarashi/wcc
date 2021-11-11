2713ccec57e9933385b7b472581cb1d22213baed
Renata <rsigarashi@hotmail.com>
2021-11-10 23:33:16 -0300

    Aula 3 - Exercicios partes 1, 2 e 3 resolvidos

---
+++ aula3/Exercicios - Parte 3/exercicio1.js
@@ -0,0 +1,14 @@
+// 1. Crie uma variável chamada `hojeVaiChover` e uma condicional `if` que imprima na tela o seguinte:
+
+// - "Leve seu guarda-chuva, hoje vai chover" se o valor for verdadeiro
+// - "Fique tranquilo, hoje será um dia se sol" se o valor for falso
+
+var hojeVaiChover = false;
+
+if(hojeVaiChover) {
+    console.log("Leve seu guarda-chuva, hoje vai chove");
+} else {
+    console.log("Fique tranquilo, hoje será um dia se sol")
+   }
+
+
---
+++ aula3/Exercicios - Parte 3/exercicio2.js
@@ -0,0 +1,63 @@
+
+// Crie variáveis para cada uma das comparações e imprima o resultado delas. 
+
+// a. 5 é maior que 1
+// b. 2000000 é menor que 3000000000
+// c. "Banana" é igual a "Banana"
+// d. "50" é estritamente igual a 50
+// e. "Carro" é diferente de "bicicleta"
+// f. 50 é maior que 27
+// g. 975 é menor ou igual a 1200
+
+// Item a
+var ehMaior = 5;
+
+ehMaior > 1 ? console.log(`${ehMaior} é maior que 1`) : console.log(`${ehMaior} é menor que 1`);
+
+
+// Item b
+var ehMenor = 2000000;
+
+if (ehMenor < 3000000000) {
+    console.log(`${ehMenor} é menor que 3000000000`)
+} else {
+    console.log(`${ehMenor} não é menor que 3000000000`)
+}
+
+
+// Item c
+var fruta = "Banana";
+fruta == "Banana" ? console.log(`"${fruta}" é igual "Banana"`) : console.log(`${fruta} não é igual "Banana"`);
+
+
+// Item d
+var num = 50;
+
+num === 50 ? console.log(`"${num}" é estritamente igual a 50`) : console.log(`${num} NÃO é estritamente igual a 50`);
+
+
+// Item e 
+var veiculo1 = "Carro";
+var veiculo2 = "bicicleta"
+
+veiculo1 == veiculo2 ? console.log(`"${veiculo1}" é igual e "${veiculo2}"`) : console.log(`"${veiculo1}" é diferente de "${veiculo2}"`);
+
+
+//Item f
+var num1 = 50;
+var num2 = 27;
+
+if(num1 > num2) {
+    console.log(`${num1} é maior que ${num2}`) 
+} else {
+    console.log(`${num1} não é maior que ${num2}`) 
+}
+
+// Item g
+var numero1 = 975
+var numero2 = 1200;
+
+numero1 <= numero2 ? console.log(`${numero1} é menor ou igual ${numero2}`) : console.log(`${numero1} é maior ou igual ${numero2}`)
+
+
+
