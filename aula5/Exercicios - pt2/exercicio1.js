600ca5d8782958eae4cb6af9d907d362b2d0c5fd
Renata <rsigarashi@hotmail.com>
2021-11-15 11:33:57 -0300

    Aula 5 (loops) - Exercicio 1 atualizado

--- aula5/Exercicios - pt2/exercicio1.js
+++ aula5/Exercicios - pt2/exercicio1.js
@@ -1,9 +1,22 @@
-const valoresNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const valoresNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function criPares() {
-    for(let i = 0; i < valoresNum.length; i++){
-        let resultPares = valoresNum[i] % 2 ===0
+// function criaPares() {
+//     for(let i = 0; i < valoresNum.length; i++){
+//         if (valoresNum[i] % 2 === 0){
+//             console.log(valoresNum[i]);
+//         }
+//     }
+// }
+// criaPares();
+
+function criaPares(valoresNum){
+    for(let cont=0; cont < valoresNum.length; cont++){
+        const meuItemAtual = valoresNum[cont]; //para ficar mais facil entender
+        if(meuItemAtual % 2 === 0){
+            console.log(valoresNum[cont]);
+        }
     }
 }
 
-criPares();
+const valoresNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+criaPares(valoresNum);
